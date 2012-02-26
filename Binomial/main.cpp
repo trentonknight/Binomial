7cc9792265602009df43cbc2a755bf41e9c029f8
Jason Mansfield <trentonknight@gmail.com>
2012-02-26 16:30:50 -0500

    First commit

--- Binomial/main.cpp
+++ Binomial/main.cpp
@@ -3,16 +3,62 @@
 //  Binomial
 //
 //  Created by Jason Mansfield on 2/26/12.
-//  Copyright 2012 Regis University. All rights reserved.
-//
+
 
 #include <iostream>
+#include <math.h>
 
-int main (int argc, const char * argv[])
+double Factorial(double fact,double lim);
+double GetBinomialCoefficient(double n, double k);
+
+using namespace std;
+
+int main ()
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    double n = 1;
+    double k = 0;
+    double binomialCoefficient = 0;
+    
+    while (n > 0) {
+        cout << "Enter object n element for C(n,k): " << endl;
+        cin >> n;
+        cout << "Enter object k-combination for C(n,k): " << endl;
+        cin >> k;
+        binomialCoefficient = GetBinomialCoefficient(n, k);
+        cout << "Binomial Coefficent: " << binomialCoefficient << endl;
+    }
+   
+          
+    
+    
+          return 0;
 }
+double Factorial(double fact,double lim){
+    double total = 0;
+    
+    do {
+        total = total + (fact * (fact - 1));
+        fact--;
+    }while((fact - 1) > lim);
+    
+    return total;
+   }
+double GetBinomialCoefficient(double n, double k){
+    
+    double lim = n - k;
+    double numerator = n;
+    double denominator = k;
+    double BC = 0;
+    
+    if(k <= 0){
+        return BC = 1;
+    }
+    else{
+    numerator = Factorial(numerator,lim);
+    denominator = Factorial(denominator,0);
+    BC = numerator/denominator;
+    return BC;
+    }
+    }
+    
 
