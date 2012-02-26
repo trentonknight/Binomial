fd7ac4c7782e41847251689205ff712cd2259ed2
Jason Mansfield <trentonknight@gmail.com>
2012-02-26 09:49:50 -0500

    Initial Commit

---
+++ Binomial/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Binomial
+//
+//  Created by Jason Mansfield on 2/26/12.
+//  Copyright 2012 Regis University. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
